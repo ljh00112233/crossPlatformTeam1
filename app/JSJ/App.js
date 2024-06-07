3ea450494faf820f11cb5b7c26ece798a0dddad8
ljh00112233 <l268728@naver.com>
2024-06-07 23:00:32 +0900

    JSJ

---
+++ app/JSJ/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SelectionGrid from './SelectionGrid';
+import MyButton from '../MyButton';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>직업사진 기억하기</Text>
+      <SelectionGrid />
+      <MyButton name={'../../List'}></MyButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+});
